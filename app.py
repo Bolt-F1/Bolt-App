599aaffb6485f00e78467faa415fe5a81bf9b32e
Bolt-F1 <12345678+Bolt-F1@users.noreply.github.com>
2025-09-06 16:41:23 +0100

    local code  errors removed

--- app.py
+++ app.py
@@ -425,8 +425,8 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 for local dev
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
 
